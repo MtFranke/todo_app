c7ed7fa0a027132d6edb49613dd31ebf4580632d
mateuszfranke <mateuszfranke@icloud.com>
2020-10-20 08:19:08 +0200

    active tab implemented

--- src/app/active/active.component.ts
+++ src/app/active/active.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Task } from '../Interfaces/Task';
 
 @Component({
   selector: 'app-active',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveComponent implements OnInit {
 
+  @Input() todoArr: Task[];
+  todo: string;
+  @Output() todoArrEvent: EventEmitter<Task[]> = new EventEmitter<Task[]>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  AddTask(): void{
+    if (this.todo !== ''){
+      this.todoArr.push({todo: this.todo, completed: false});
+      this.todoArrEvent.emit(this.todoArr);
+    }
+  }
+  TaskCompleted(i: number): void{
+    if (this.todoArr[i].completed === true)
+    {
+      this.todoArr[i].completed = false;
+      this.todoArrEvent.emit(this.todoArr);
+
+    }else {
+      this.todoArr[i].completed = true;
+      this.todoArrEvent.emit(this.todoArr);
+
+    }
+  }
+
 
 }
